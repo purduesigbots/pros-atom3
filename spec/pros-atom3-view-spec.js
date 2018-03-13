d66495b5377c9cf6a3d0a0317c8f408f9beb4d62
Alex Brooke <akb.sbc@gmail.com>
2018-03-12 23:06:45 -0400

    initial commit

---
+++ spec/pros-atom3-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ProsAtom3View from '../lib/pros-atom3-view';
+
+describe('ProsAtom3View', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
